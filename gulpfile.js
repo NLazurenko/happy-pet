127a618448ffe464ab26d680096a50eb40979533
Nikita Lazurenko <naj040315@gmail.com>
2020-08-07 14:25:54 +0300

    init

--- gulpfile.js
+++ gulpfile.js
@@ -61,10 +61,10 @@ gulp.task("browser-sync", function () {
 gulp.task("watch", function () {
 	gulp.watch("app/scss/**/*.scss", gulp.parallel("sass"));
 	gulp.watch("app/*.html", gulp.parallel("html"));
-	gulp.watch("app/js/*.js", gulp.parallel("js"));
+	// gulp.watch("app/js/*.js", gulp.parallel("js"));
 });
 
 gulp.task(
 	"default",
-	gulp.parallel("sass", "watch", "browser-sync", "script", "style")
+	gulp.parallel("sass", "watch", "browser-sync", "style")
 );
